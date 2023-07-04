4014897a37eceebb18200609141209d7baa0938c
user <malispierre007@gmail.com>
2023-07-04 18:26:20 +0200

    init 5

--- backend/core/settings/dev.py
+++ backend/core/settings/dev.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = True
+DEBUG = config('DEBUG')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
@@ -52,13 +52,14 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 ALLOWED_HOSTS = ['*']
-
+'''
 ALLOWED_HOSTS.extend(
     filter(
         None,
         config('ALLOWED_HOSTS', '').split(','),
         )
 )
+'''
 
 
 # Application definition
