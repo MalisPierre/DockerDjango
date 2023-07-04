96d771248ff6804f0fdff644a12ebab15950b754
user <malispierre007@gmail.com>
2023-07-04 18:44:24 +0200

    init 6

---
+++ frontend/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+ <div class="hello">
+    <div style="padding:40px;">
+      <h1>
+        Hello from
+        <span style="color:#29558C">Middleware[i</span>
+        <span style="color:#AD5353">]nventory</span>
+      </h1>
+      <p>
+        It is good to have you here this is a Simple Page in
+        <span class="bg-info">Vue JS</span> and Running on
+        <span class="bg-success">Docker Container</span>
+      </p>
+    </div>
+  </div>
+  <router-view/>
+</template>
+<script>
+window.Vue = require("vue");
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ frontend/src/views/FilmCategListView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="about">
+    <h1>List of films:</h1>
+    <ul>
+      <li v-for="item in listItems">
+        {{ item.title }}
+      </li>
+    </ul>
+    enddde
+  </div>
+
+</template>
+
+<!--
+<script>
+  export default {
+    data() {
+      return {
+        listItems: []
+      }
+    },
+    methods: {
+      async getData() {
+        const res = await fetch("localhost:8000/api_cinema/list/");
+        const finalRes = await res.json();
+        this.listItems = finalRes;
+        console.log("DATA = " + finalRes);
+      }
+    },
+    mounted() {
+      this.getData()
+    }
+  }
+</script>
+-->
+
+<script setup>
+  import { ref } from 'vue';
+
+  const listItems = ref([]);
+
+  async function getData() {
+    const res = await fetch("http://localhost:8000/api_cinema/list/");
+    const finalRes = await res.json();
+    listItems.value = finalRes;
+  }
+
+ getData()
+</script>
