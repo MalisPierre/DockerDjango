287464f37d98f9ce4eb0b10182b93271bc3816f9
user <malispierre007@gmail.com>
2023-07-05 05:34:25 +0200

    init 9

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,13 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    allowedHosts: [
-      'localhost',
-      'https://frontend-0aaz.onrender.com/',
-      '3.134.238.10',
-      '3.129.111.220',
-      '52.15.118.168',
-      '192.168.1.1',
-    ],
+    allowedHosts: 'all',
     }
 })
